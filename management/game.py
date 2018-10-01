93065b25313402fb37602c5702a89f7d59ba4147
Unknown <bramspammail003@gmail.com>
2018-10-01 23:31:53 +0200

    Add files for database management

---
+++ management/game.py
@@ -0,0 +1,6 @@
+def signup(user_id,emoji) -> bool:
+    # This function signs up a new user for a game.
+    # One must make sure the Game Master have opened the signups.
+    # Users are signed up as spectators by default.
+    
+    return False # TODO
